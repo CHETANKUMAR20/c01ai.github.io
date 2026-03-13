c0a57c7641088e41d082fbce25e2933476cd0117
CHETANKUMAR20 <chetankumar.build@gmail.com>
2026-03-13 20:33:38 +0530

    removed old site files

--- js/sidebar.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("menuToggle")
-const sidebar = document.getElementById("sidebar")
-
-toggle.addEventListener("click", () => {
-sidebar.classList.toggle("active")
-})
